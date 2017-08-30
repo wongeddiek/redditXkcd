2336f9498a3f4fe715e602866ebecf21930809ef
Edward Wong <wong.eddiek@gmail.com>
2017-08-29 20:20:55 -0600

    completed xkcd reddit bot

--- app.js
+++ app.js
@@ -12,30 +12,6 @@ const snoowrap = require('snoowrap');
 //using snoostorm for reddit comment streams
 const snoostorm = require("snoostorm");
 
-//request function to get explainxkcd html data
-function urlRequest(url, cFunction) {
-  request(url, function(error, response, body){
-    cFunction(error, response, body);
-  });
-}
-
-var xkcdData ='';
-
-xkcdData = urlRequest('https://www.explainxkcd.com/356', function(error, response, body){
-  if (error) {
-    console.log('error: ' + error);
-  }
-  const $ = cheerio.load(body);
-  var data = '';
-  $('p').each( function() {
-    data += $(this).text() + '\n';
-    if ($(this).next().prop('tagName') !== 'P') {
-      return false;
-    }
-  });
-  return data;
-});
-
 //creating client credentials
 const clientCred = {
   userAgent: 'thunzBot',
@@ -48,9 +24,6 @@ const clientCred = {
 //using snoowrap
 const r = new snoowrap(clientCred);
 
-//getting the test subreddit
-// const subreddit = r.getSubreddit('test');
-
 //using snoostorm for commentstream
 var client = new snoostorm(r);
 
@@ -60,41 +33,47 @@ var commentStream = client.CommentStream({
   "pollTime": 2000           // Time in between polls in milliseconds, defaults to 2000, 30 times a minute, in accordance with Reddit's 60req/min, allowing you to perform both comment and submission updates. Note that snoowrap will automatically wait to be in compliance with Reddit's Guidelines
 });
 
+//request function to get explainxkcd html data
+function urlRequest(url, cFunction) {
+  request(url, function(error, response, body){
+    cFunction(error, response, body);
+  });
+}
+
+//header and footer to be appended with the explainxkcd comic comment reply
+var header = "**I'm a bot, beep boop.  Explanation of this xkcd:**" + '\n';
+var footer = '\n*---Comic explanation extracted from [explainxkcd](http://www.explainxkcd.com) | Bot created by u/thunderlordz *';
+
 commentStream.on("comment", function(comment) {
   console.log(`New comment by ${comment.author.name}`);
-  if (comment.body.includes('thunzBot test')) {
-    comment.reply('test bot in action!');
+  //regular expression pattern for the xkcd url
+  var regexPatt = /[a-zA-z]*[0-9]*https*:\/\/www.xkcd.com\/[0-9]*/;
+  if (regexPatt.test(comment.body)) {
+    console.log('matched!');
+    //getting the xkcd url from comment body
+    var xkcd_url = comment.body.match(regexPatt)[0];
+    //getting the comic number (number after the /) from xkcd url
+    var urlNumber = Number(xkcd_url.match(/(?:\/)([0-9]+)/)[1]);
+    //compiling the explainxkcd url
+    var expxkcd_url = `https://www.explainxkcd.com/${urlNumber}`;
+
+    //calling the urlRequest function to get the explainxkcd data
+    urlRequest(expxkcd_url, function(error, response, body){
+      if (error) {
+        console.log('error: ' + error);
+      }
+      //load the html DOM to cheerio
+      const $ = cheerio.load(body);
+      var data = '';
+      //compile the <p> tag text in the html
+      $('p').each( function() {
+        data += $(this).text() + '\n';
+        if ($(this).next().prop('tagName') !== 'P') {
+          return false;
+        }
+      });
+      //send completed data as reddit comment
+      comment.reply(header + data + footer);
+    });
   }
 });
-
-//using raw.js
-//authenticate Reddit using OAuth2
-// reddit.setupOAuth2("3bC-WyZVJOJVJw", "Upp9fSXTM8svRrbyZA4Aa4Htf4s", "http://localhost:8080");
-//
-// reddit.auth({"username": "thunzBot", "password": "array!bot123"}, function(err, response) {
-//   if(err) {
-//     console.log("Unable to authenticate user: " + err);
-//   } else {
-//     reddit.me(function(err,response){
-//
-//       console.log(response.name + ' is authenticated');
-//       //starts pulling comments after reddit authentication
-//       console.log('getting reddit comments');
-//
-//       reddit.comments({"r": 'test', "limit": 100}, function(error, comments){
-//         if(error) {
-//           console.log('unable to get comments: ' + error);
-//         } else {
-//           //going through each comment body
-//           comments.data.children.forEach(function(element){
-//             console.log(element.data.link_url);
-//             // if(element.data.body.includes("https://www.xkcd.com")){
-//             //   console.log('match found! \n');
-//             // }
-//           });
-//           // console.log(comments.data.children[0].data);
-//         }
-//       });
-//     });
-//   }
-// });
